27fa503106a56d107a1b5f9f45a533c8d476dd11
Kris Kamau <Kriskamau6@gmail.com>
2018-08-30 22:11:52 -0400

    Added routes and static files

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
 
-//controller imports
 import routes from './src/routes/crmRoutes';
 
 const app = express();
 const PORT = 3000;
 
-//initializing routes
+// initializing mongoose
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/CRMdb', {
+    useNewUrlParser: true
+});
+
+// bodyparser setup
+app.use(bodyParser.urlencoded( { extended: true }));
+app.use(bodyParser.json())
+
+// initializing routes
 routes(app);
+
+// serving static files
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.send(`Node and Express server running on port ${PORT}`);
